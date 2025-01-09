b1a7ee630c1c3af4db66988c15f4b642e2efc109
Josh Everett <josh.r.everett@gmail.com>
2025-01-08 19:52:39 -0800

    fix: tools must be optional

--- app/types/vapi.py
+++ app/types/vapi.py
@@ -60,7 +60,7 @@ class ChatRequest(BaseModel):
     model: str
     messages: List[Message]
     temperature: float
-    tools: List[Tool]
+    tools: Optional[List[Tool]] = None
     stream: bool
     max_tokens: int
     call: Call
