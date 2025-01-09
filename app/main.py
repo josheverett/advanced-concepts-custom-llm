e07a489a3dec4a0ffee73c935e4b84741eefa16f
Josh Everett <josh.r.everett@gmail.com>
2025-01-09 11:38:26 -0800

    feat: working 'please hold' demo

--- app/main.py
+++ app/main.py
@@ -9,18 +9,39 @@ from starlette.responses import StreamingResponse
 from app.types.vapi import ChatRequest
 from dotenv import load_dotenv
 
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 load_dotenv()
 
 app = FastAPI()
 
 client = AsyncOpenAI(api_key=os.environ["OPENAI_API_KEY"])
 
-# This modification to the Custom LLM sample project demonstrates an issue with streaming chat.completion.chunks when
-# the end token is not received within 20 seconds. Note that the Web SDK was used for this test.
-#
-# All chunks are streamed in real time to the client as expected. When the end token is received, this is reflected in
-# the logs, but the logs do not show a subsequent "Voice input" log, nor is there a corresponding "voice-input" event
-# in the client.
+def get_flush_chunk(id: str, created: int):
+    return {
+        'id': id,
+        'choices': [{
+            'delta': {
+                'content': "<flush>",
+                'function_call': None,
+                'refusal': None,
+                'role': None,
+                'tool_calls': None
+            },
+            'finish_reason': None,
+            'index': 0,
+            'logprobs': None
+        }],
+        'created': created,
+        'model': 'gpt-3.5-turbo-0125',
+        'object': 'chat.completion.chunk',
+        'service_tier': None,
+        'system_fingerprint': None,
+        'usage': None
+    }
 
 @app.post("/chat/completions")
 async def chat_completion_stream(vapi_payload: ChatRequest): # Note: Ignoring the payload for this demonstration.
@@ -42,7 +63,31 @@ async def chat_completion_stream(vapi_payload: ChatRequest): # Note: Ignoring th
             messages=[
                 {
                     "role": "user",
-                    "content": "Say: 'Thank you for holding.'",
+                    "content": "Say: 'Thank you for continuing to hold.'",
+                }
+            ],
+            temperature=0,
+            stream=True,
+        )
+
+        response3 = await client.chat.completions.create(
+            model="gpt-3.5-turbo-0125",
+            messages=[
+                {
+                    "role": "user",
+                    "content": "Say: 'Thank you for your patience. Here is what I found.'",
+                }
+            ],
+            temperature=0,
+            stream=True,
+        )
+
+        response4 = await client.chat.completions.create(
+            model="gpt-3.5-turbo-0125",
+            messages=[
+                {
+                    "role": "user",
+                    "content": "Say: 'Have a great day!'",
                 }
             ],
             temperature=0,
@@ -51,19 +96,44 @@ async def chat_completion_stream(vapi_payload: ChatRequest): # Note: Ignoring th
 
         async def event_stream():
             try:
-                # Wait for 10 seconds to make sure we exhaust the 20s limit.
-                await asyncio.sleep(10)
-
+                logger.info("sending chunks for response 1")
                 async for chunk in response1:
+                    is_last_chunk = chunk.choices[0].finish_reason is not None
+                    if is_last_chunk:
+                        yield f"data: {json.dumps(get_flush_chunk(chunk.id, chunk.created))}\n\n"
+                    logger.info(f"sending chunk for response 1: {chunk.model_dump()}")
                     yield f"data: {json.dumps(chunk.model_dump())}\n\n"
 
-                # Wait another 10 seconds.
                 await asyncio.sleep(10)
 
+                logger.info("sending chunks for response 2")
                 async for chunk in response2:
+                    is_last_chunk = chunk.choices[0].finish_reason is not None
+                    if is_last_chunk:
+                        yield f"data: {json.dumps(get_flush_chunk(chunk.id, chunk.created))}\n\n"
+                    logger.info(f"sending chunk for response 2: {chunk.model_dump()}")
                     yield f"data: {json.dumps(chunk.model_dump())}\n\n"
 
-                # Wait another 10 seconds.
+                await asyncio.sleep(10)
+
+                logger.info("sending chunks for response 3")
+                async for chunk in response3:
+                    is_last_chunk = chunk.choices[0].finish_reason is not None
+                    if is_last_chunk:
+                        yield f"data: {json.dumps(get_flush_chunk(chunk.id, chunk.created))}\n\n"
+                    logger.info(f"sending chunk for response 3: {chunk.model_dump()}")
+                    yield f"data: {json.dumps(chunk.model_dump())}\n\n"
+
+                await asyncio.sleep(10)
+
+                logger.info("sending chunks for response 4")
+                async for chunk in response4:
+                    is_last_chunk = chunk.choices[0].finish_reason is not None
+                    if is_last_chunk:
+                        yield f"data: {json.dumps(get_flush_chunk(chunk.id, chunk.created))}\n\n"
+                    logger.info(f"sending chunk for response 4: {chunk.model_dump()}")
+                    yield f"data: {json.dumps(chunk.model_dump())}\n\n"
+
                 await asyncio.sleep(10)
 
                 # Send end token.
